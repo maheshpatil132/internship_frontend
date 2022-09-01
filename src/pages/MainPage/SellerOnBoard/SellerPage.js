de82c6f678b3cdefbc29d021cc79e1430c339ef2
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-09-01 17:23:27 +0530

    added routing

--- src/pages/MainPage/SellerOnBoard/SellerPage.js
+++ src/pages/MainPage/SellerOnBoard/SellerPage.js
@@ -4,28 +4,33 @@ import img from '../../../images/onboard.png'
 
 const SellerPage = () => {
     return (
-        <div className=' px-20 py-2 h-screen flex flex-col'>
+        <div className=' relative px-20 py-2 h-screen flex flex-col overflow-hidden'>
             <MainHeading />
             <div className=' flex-1 flex justify-between items-center'>
                 <div className="left flex flex-col gap-6 w-2/5">
-                    <h1 className=' font-bold text-3xl'>Join Maqure <br />
+                    <h1 className=' font-semibold text-4xl'>Join Maqure <br />
                         To Sell Nationally and <br /> Internationally</h1>
 
-                    <p className=' text-buyer-text-color'>Request a demo to learn how Maqure makes it easier than ever for you to market, sell and transact online.
+                    <p className=' text-base text-buyer-text-color'>Request a demo to learn how Maqure makes it easier than ever for you to market, sell and transact online.
                     </p>
                     <div className=' w-96'>
-                        <form action="" className=' flex flex-col gap-3'>
-                            <input type="text" className=' rounded py-2 px-4 border outline-none' placeholder='Company Name' />
-                            <input type="text" className=' py-2 px-4 border outline-none' placeholder='Email ' />
-                            <input type="text" className=' py-2 px-4 border outline-none' placeholder='Phone Number' />
+                        <form action="" className=' flex flex-col gap-4'>
+                            <input type="text" className=' border-[#667080] rounded py-2 px-4 border outline-none' placeholder='Company Name' />
+                            <input type="text" className=' border-[#667080] py-2 px-4 border outline-none' placeholder='Email ' />
+                            <input type="text" className=' border-[#667080] py-2 px-4 border outline-none' placeholder='Phone Number' />
                             <button className=' bg-buyer-primary py-2 px-6 text-white rounded'>Request A Call </button>
                         </form>
                     </div>
                 </div>
                 <div className="right flex">
-                    <img className=' h-[32rem]' src={img} alt="" />
+                    <img className='h-[29rem]' src={img} alt="" />
                 </div>
             </div>
+           
+           <div className='main_seller'>
+                 
+           </div>
+    
         </div>
     )
 }
