69d4931e1dfaf202ac611c42ccd8d800bdc9f30d
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-09-01 17:40:06 +0530

    added routing

--- src/pages/MainPage/SellerOnBoard/SellerPage.js
+++ src/pages/MainPage/SellerOnBoard/SellerPage.js
@@ -4,33 +4,42 @@ import img from '../../../images/onboard.png'
 
 const SellerPage = () => {
     return (
-        <div className=' relative px-20 py-2 h-screen flex flex-col overflow-hidden'>
-            <MainHeading />
-            <div className=' flex-1 flex justify-between items-center'>
-                <div className="left flex flex-col gap-6 w-2/5">
-                    <h1 className=' font-semibold text-4xl'>Join Maqure <br />
-                        To Sell Nationally and <br /> Internationally</h1>
+        <div className=' flex flex-col'>
 
-                    <p className=' text-base text-buyer-text-color'>Request a demo to learn how Maqure makes it easier than ever for you to market, sell and transact online.
-                    </p>
-                    <div className=' w-96'>
-                        <form action="" className=' flex flex-col gap-4'>
-                            <input type="text" className=' border-[#667080] rounded py-2 px-4 border outline-none' placeholder='Company Name' />
-                            <input type="text" className=' border-[#667080] py-2 px-4 border outline-none' placeholder='Email ' />
-                            <input type="text" className=' border-[#667080] py-2 px-4 border outline-none' placeholder='Phone Number' />
-                            <button className=' bg-buyer-primary py-2 px-6 text-white rounded'>Request A Call </button>
-                        </form>
+            {/* home div */}
+            <div className=' relative px-20 py-2 h-screen flex flex-col overflow-hidden'>
+                <MainHeading />
+                <div className=' flex-1 flex justify-between items-center'>
+                    <div className="left flex flex-col gap-6 w-2/5">
+                        <h1 className=' font-semibold text-4xl text-[#00212F]'>Join Maqure <br />
+                            To Sell Nationally and <br /> Internationally</h1>
+
+                        <p className=' text-base text-buyer-text-color'>Request a demo to learn how Maqure makes it easier than ever for you to market, sell and transact online.
+                        </p>
+                        <div className=' w-96'>
+                            <form action="" className=' flex flex-col gap-4'>
+                                <input type="text" className=' border-[#667080] rounded py-2 px-4 border outline-none' placeholder='Company Name' />
+                                <input type="text" className=' border-[#667080] py-2 px-4 border outline-none' placeholder='Email ' />
+                                <input type="text" className=' border-[#667080] py-2 px-4 border outline-none' placeholder='Phone Number' />
+                                <button className=' bg-buyer-primary py-2 px-6 text-white rounded'>Request A Call </button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="right flex">
+                        <img className='h-[29rem]' src={img} alt="" />
                     </div>
                 </div>
-                <div className="right flex">
-                    <img className='h-[29rem]' src={img} alt="" />
+
+                <div className='main_seller'>
+
                 </div>
+
             </div>
-           
-           <div className='main_seller'>
-                 
-           </div>
-    
+
+             {/* trusted.....part */}
+             <div>
+                <h1 className=''>TRUSTED BY INDUSTRY VETRANS</h1>
+             </div>
         </div>
     )
 }
