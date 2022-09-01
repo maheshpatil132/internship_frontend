5b5aeaea39e064ce81d82bc0b39bc3b14c089c4f
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-09-01 14:28:10 +0530

    added routing

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,32 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsCartFill } from 'react-icons/bs'
+import {AiFillQuestionCircle} from 'react-icons/ai'
+import {IoArrowBackCircleSharp} from 'react-icons/io5'
+import {BsCloudUploadFill} from 'react-icons/bs'
 
 const OrderForm = () => {
 
-
+   const [form1, setForm1] = useState(true)
 
 
     //functions
-
+    
+    const forward = (e) =>{
+        e.preventDefault();
+        setForm1(!form1)
+    }
 
     return (
         <div className='px-1 mt-11  '>
             <div className=' rounded flex  order_shadow order_form overflow-hidden relative bg-white '>
-            <form action="" className=' flex flex-col space-y-3 py-4 px-6 '>
-                <div className=' flex items-center gap-4 text-buyer-primary font-medium'>
-                    <BsCartFill size={20} />
-                    <h1>Order Now</h1>
-                </div>
-                <p className=' text-buyer-text-color '>You can order this product right now by requestion a quote from us </p>
-                <div>
-                    <h3 className='mb-1'>Quantity</h3>
-                    <div className="input_cover w-full flex gap-4">
-                        <div className=' flex-1'>
-                            <input type="text" placeholder='Enter the Volume' className=' w-full box_shadow outline-none border p-2 px-4' />
-                        </div>
+                <form action="" onSubmit={forward} className={` ${!form1 && 'left_form'} left-0 transition ease-in-out delay-150  flex flex-col justify-between  py-4 px-6 `}>
+                    <div className=' flex items-center gap-4 text-buyer-primary font-medium'>
+                        <BsCartFill size={20} />
+                        <h1>Order Now</h1>
+                    </div>
+                    <p className=' text-buyer-text-color '>You can order this product right now by requestion a quote from us </p>
+                    <div>
+                        <h3 className='mb-1'>Quantity</h3>
+                        <div className="input_cover w-full flex gap-4">
+                            <div className=' flex-1'>
+                                <input type="text" placeholder='Enter the Volume' className=' w-full box_shadow outline-none border p-2 px-4' />
+                            </div>
 
-                        <div className="flex box_shadow justify-center border px-3">
-                            <select className="form-select 
+                            <div className="flex box_shadow justify-center border px-3">
+                                <select className="form-select 
                                             px-2.5
                                             py-1.5
                                             w-full
@@ -37,30 +44,54 @@ const OrderForm = () => {
                                             ease-in-out
                                             outline-none
                                             ">
-                                <option value="1">kgs</option>
-                                <option value="1">Two</option>
-                                <option value="1">Thre</option>
-                            </select>
+                                    <option value="1">kgs</option>
+                                    <option value="1">Two</option>
+                                    <option value="1">Thre</option>
+                                </select>
+                            </div>
+
                         </div>
-
                     </div>
-                </div>
-                <div>
-                    <h3 className=' mb-1'>Pincode</h3>
-                    <div className="input_cover flex flex-1 gap-4">
-                        <input type="text" placeholder='Enter Delivery Pincode' className='outline-none w-full box_shadow  border p-2 px-4' />
+                    <div>
+                        <h3 className=' mb-1'>Pincode</h3>
+                        <div className="input_cover flex flex-1 gap-4">
+                            <input type="text" placeholder='Enter Delivery Pincode' className='outline-none w-full box_shadow  border p-2 px-4' />
+                        </div>
                     </div>
-                </div>
-                <p className='text-buyer-text-color'>Final Price will be available upon quotes</p>
-                <button className=' button bg-buyer-primary py-2 px-4 rounded text-white'>Request a Quote</button>
-            </form>
+                    <p className='text-buyer-text-color'>Final Price will be available upon quotes</p>
+                    <button  className=' button bg-buyer-primary py-2 px-4 rounded text-white'>Request a Quote</button>
+                </form>
 
-            <form action="" className=' right_form left-full flex flex-col space-y-3 py-5 px-6  text-sm'>
-            <div className=' flex items-center gap-4 text-buyer-primary font-medium'>
-                    <BsCartFill size={20} />
-                    <h1>Order Now</h1>
-                </div>
-            </form>
+                <form action="" className={`  ${form1 && 'right_form'} left-full flex flex-col justify-between gap-1 py-4 px-6`}>
+                    <div className=' flex items-center gap-2 text-buyer-primary font-medium'>
+                        <IoArrowBackCircleSharp onClick={forward} className='cursor-pointer' size={22} />
+                        <h1>Order Now</h1>
+                    </div>
+                    <p className=' text-buyer-text-color '>You can order this product right now by requestion a quote from us </p>
+
+                    {/* Additional Remarks */}
+                    <div className=' flex flex-col gap-1'>
+                        <h3>Additional Remarks </h3>
+                        <input type="text" placeholder='Enter your Requirements ' className='rounded border py-2 px-2' />
+                    </div>
+
+                    {/* upload documents */}
+                    <div className=' flex flex-col gap-1'>
+
+                    {/* upload document heading */}
+                    <div className=' flex items-center gap-2'>
+                       <h1 className=' '>Upload Documents  </h1>
+                       <AiFillQuestionCircle size={18}/>
+                    </div>
+
+                    <div className=' bg-[#1672DE] w-fit flex text-white text-[0.7rem] rounded flex-col gap-2  p-2 items-center justify-center px-2'>
+                       <BsCloudUploadFill color='' size={20}/>
+                       <p className=' '>Upload</p>
+                    </div>
+                    </div>
+                    <p className=' text-buyer-text-color'>Final Price will be available upon quotes</p>
+                    <button  className=' bg-buyer-primary py-2 text-white rounded-md '>submit</button>
+                </form>
             </div>
 
         </div>
--- src/components/Prodcut.js
+++ src/components/Prodcut.js
@@ -11,7 +11,7 @@ const Prodcut = () => {
        <div className=' product flex-1 h-screen overflow-y-scroll '>
          <ProdDecr/>
        </div>
-       <div className=' text-[0.82rem] px-3 py-4 flex flex-col justify-between h-screen fixed right-4'>
+       <div className=' text-[0.84rem] px-3 py-4 flex flex-col justify-between h-screen fixed right-4'>
           <OrderForm/>
           <Featues/>
        </div>
