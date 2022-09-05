77772373793b24c3e34233bf2e0820ef98026817
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-09-05 16:59:43 +0530

    added routing

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,11 +3,11 @@ import map from '../images/map.png'
 const Map = () => {
   return (
     <div className='flex items-center px-20 py-10 justify-around bg-[#00212F] text-white'>
-      <div className=" flex-1 space-y-7">
-        <h1 className=' font-semibold text-3xl'>Skyrocket your Reach Across the world with Maqure! </h1>
-        <p className=' text-base text-[#FBFBFB]'>Request a demo to learn how Maqure makes it easier than ever for you to market, sell and transact online.
+      <div className=" flex-1 ">
+        <h1 className=' mb-5 font-semibold text-3xl'>Skyrocket your Reach Across the world with Maqure! </h1>
+        <p className=' text-base mb-12 text-[#FBFBFB]'>Request a demo to learn how Maqure makes it easier than ever for you to market, sell and transact online.
         </p>
-        <button className=' p-2 px-4 w-[345px] rounded-md bg-white'> <span className=' font-semibold text-lg text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'> Request A Call </span></button>
+        <button className=' p-2 px-4 w-[250px] rounded-md bg-white'> <span className=' font-semibold text-lg text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'> Request A Call </span></button>
       </div>
       
       <div className=" w-[33rem]">
