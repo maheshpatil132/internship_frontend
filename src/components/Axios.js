8b96533be0e2b44ebe6a6b6f3d12f68d854c2ba6
mahesh patil <92781925+maheshpatil132@users.noreply.github.com>
2022-10-11 13:54:18 +0530

    build

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 export const Axios = axios.create({
-    baseURL: 'https://maqure.43.205.71.30.nip.io',
+    baseURL: 'https://maqure.43.205.71.30.nip.io/',
     headers: {
       "Content-Type": "application/json",
-    }
+    },
+    withCredentials:true
   });
   
